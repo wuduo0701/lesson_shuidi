9082ef2dbe99aa5b63cf7e6239b90a421adacf93
1970186643 <1970186643@qq.com>
2019-12-01 22:26:20 +0800

    didi

---
+++ mini_program/didi/pages/index/index.js
@@ -0,0 +1,43 @@
+const app = getApp();
+Page({
+  data:{
+    currentTab: 3,
+    navScrollLeft: 0,
+    navData: [
+      {
+        id: 1,
+        name: '专车'
+      },
+      {
+        id: 2,
+        name: '快车'
+      }
+    ]
+  },
+  showUser() {
+    console.log(app);
+    // 用户如何点击了授权，就显示小家如果没有，则取登入
+    wx.navigateTo({
+      url:"/pages/login/login"
+    })
+  },
+  onLoad (){
+    wx.request({
+      url: 'http://localhost:1314/indexPage',
+      success: (res) =>{
+        // console.log(res);
+        const navData = res.data.navData;
+        this.setData({
+          navData
+        })
+      }
+    })
+  },
+  switchNav(e){
+    const cur = e.currentTarget.dataset.current;
+    // console.log(cur);
+    this.setData({
+      currentTab: cur
+    })
+  }
+})
