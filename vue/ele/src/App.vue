f004689c0fae224725f8ae880955fe46572760e8
1970186643 <1970186643@qq.com>
2020-02-26 17:16:49 +0800

    modified

--- vue/ele/src/App.vue
+++ vue/ele/src/App.vue
@@ -1,32 +1,45 @@
 <template>
   <div id="app">
+    <!-- <div style="width:16rem;height:2rem;background-color:red;"></div>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/> -->
+    <!-- 首页 -->
+    <!-- <head-top headTitle="东华理工大学广兰校区">
+      <span class="head_logo" slot="logo">ele.me</span>
+    </head-top> -->
+    <!--地址 -->
+    <head-top head-title="编辑地址" go-back="true">
+      <!-- <span class="head_logo" slot="logo">ele.me</span> -->
+      <span class="edit" slot="edit">编辑</span>
+    </head-top>
   </div>
 </template>
+<script>
+import Header from './components/header/header';
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+export default {
+  components: {
+    // 怎么解决？
+    "head-top": Header
+  }  
 }
+</script>
+<style lang="stylus">
+@import "./style/common.styl"
+@import "./style/mixin.styl"
 
-#nav {
-  padding: 30px;
-}
+.head_logo
+  left .4rem 
+  font-weight 400
+  sc(0.7rem, #fff)
+  wh(2.3rem, 0.7rem)
+  ct()
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+.edit
+  right 0.4rem
+  sc(0.7rem, #fff)
+  ct()
 </style>
