4803b318af6ae701ff8c0a5f9a31bfc75c283f36
1970186643 <1970186643@qq.com>
2020-03-02 11:08:26 +0800

    js闭包

---
+++ js/bibao/private.js
@@ -0,0 +1,26 @@
+// 通过闭包实现私有方法
+//匿名函数，立即执行函数
+var Counter = (function (){
+  var privateCounter = 0;
+  function change(val){
+    privateCounter += val;
+  };
+  return{
+    increment: function(){
+      change(1);
+    },
+    decrement: function(){
+      change(-1);
+    },
+    value: function(){
+      return privateCounter;
+    }
+  }
+})();
+// 创建的是相同的词法作用域
+console.log(Counter.value());
+Counter.increment();
+Counter.increment();
+console.log(Counter.value());
+Counter.decrement();
+console.log(Counter.value());
