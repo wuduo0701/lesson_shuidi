3413f050b2d6aa2e4371b517ec60c64f60c5a9ce
1970186643 <1970186643@qq.com>
2020-03-03 22:21:22 +0800

    modified

--- vue/ele_unit/node_es6_mod_test/app.js
+++
@@ -1,7 +0,0 @@
-// const express = require('express'); //es5 支持的commonjs
-import express from 'express'; //es6模块化
-const app = express(); //部分支持 es6
-// app.use()
-app.listen("1234", () => {
-  console.log('api 服务器上线了');
-})
--- vue/ele_unit/node_es6_mod_test/index.js
+++
@@ -1,3 +0,0 @@
-// 唯一的入口，做唯一一次es5模块化
-require('babel-core/register');
-require('./app.js')
