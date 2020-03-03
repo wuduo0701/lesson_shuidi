2fe4048404d6fc5378f85568cc6e8ae5a0d81c98
1970186643 <1970186643@qq.com>
2020-03-03 13:49:54 +0800

    api

--- vue/ele_unit/ele-node/index.js
+++ vue/ele_unit/ele-node/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
-app.listen("3000",() =>{
+
+// 路由的模块化
+const router = require('./routes/index.js');
+router(app);
+
+// app.use()
+app.listen("3000", () => {
   console.log('api 服务器上线了');
 })
