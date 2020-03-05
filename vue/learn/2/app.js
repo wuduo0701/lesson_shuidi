178995ad2ce13e54950206987412dbf4b9a55b78
1970186643 <1970186643@qq.com>
2020-03-05 21:57:41 +0800

    modified

--- vue/learn/2/app.js
+++ vue/learn/2/app.js
@@ -3,7 +3,19 @@ new Vue({
   el: '#app',
   data:{
     changeColor: false,
-    changeLength: false
+    changeLength: false,
+    error: false,
+    success: false,
+    users:[
+      {name:"lin",age:"18"},
+      {name:"jia",age:"19"},
+      {name:"hao",age:"20"}
+    ],
+    object:{
+      name:"朵雾",
+      title:"今年20岁",
+    },
+    numbers: [ 1, 2, 3, 4, 5 ]
   },
   methods:{
     // a:function(){
@@ -14,7 +26,10 @@ new Vue({
         changeColor: this.changeColor,
         changeLength: this.changeLength
       }
-    }
+    },
+    // push(){
+    //   app.items.push
+    // }
   },
   computed:{
     // compClasses:function(){
@@ -22,6 +37,15 @@ new Vue({
     //     changeColor: this.changeColor
     //   }
     // }
-
+    // evenNumbers:function(){
+    //   return this.numbers.fliter(number =>{
+    //     return number % 2 === 0
+    //   })
+    // },
+    // evenNumbers: function () {
+    //   return this.numbers.filter(function (number) {
+    //     return number % 2 === 0
+    //   })
+    // }
   }
 })
--- vue/learn/2/readme.md
+++ vue/learn/2/readme.md
@@ -4,3 +4,17 @@ v-bind:class 即 :class
 可以用在实际项目中的类动态改变
 
 methods 和computed在实际调用函数时不同，methods需要加()
+
+- v-if 与 v-show的不同
+  使用v-if时dom节点不存在,被完全屏蔽了,即会变成-> <!---->
+  v-show时dom节点还存在，只是加了css样式display:none
+
+- v-for条件渲染
+  - 渲染数组时
+    可提供两个参数{{item,index}} in items
+    item:数组每一个值， index数组的下标 items为数组名
+  - 渲染对象时
+    可提供三个参数{{value,name,index}} in object
+    value 对象里的属性 ，name即键名 index对象的下标
+  最好提供key值--v-key 即 :key
+  以便它能跟踪每个节点的身份，从而重用和重新排序现有元素
