badbb048f40490295fbf887179348f3ac71f09b5
1970186643 <1970186643@qq.com>
2020-03-19 21:21:25 +0800

    modify

--- vue/ele/src/main.js
+++ vue/ele/src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './config/rem'
-
+// import ljhui from 'ljhui'
+// import 'duowu/lib/duowui.css'
 Vue.config.productionTip = false
 
+// Vue.use(ljhui)
 new Vue({
   router,
   render: h => h(App)
--- vue/lazy-img/src/App.vue
+++ vue/lazy-img/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <x-button type="primary">button</x-button> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -13,7 +14,7 @@ export default {
   data(){
     return{
       images: [
-        
+
       ]
     }
   },
