83fc22798eacc2b66978e817ffecad2d9d4f2306
1970186643 <1970186643@qq.com>
2020-03-25 23:58:50 +0800

    modified

--- vue/vuex-shop/src/components/HelloWorld.vue
+++ vue/vuex-shop/src/components/HelloWorld.vue
@@ -10,6 +10,7 @@
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
--- vue/waimai/src/components/food/food.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <div>food</div>
-</template>
