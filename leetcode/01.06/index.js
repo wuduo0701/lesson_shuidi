4f0958a5874299582748fd4240f1ef9d269926c5
1970186643 <1970186643@qq.com>
2020-03-29 23:23:54 +0800

    modify

--- leetcode/01.06/index.js
+++ leetcode/01.06/index.js
@@ -2,7 +2,7 @@ var s1 = "aabcccccaaa",
     s2 = "abbccd";
 var compressString = function(S) {
   const len = S.length; 
-  let count = 1, change = S[0];
+  let count = 1, change = S[0]; 
   let s = "";
   for(let i = 1;i< len; ++i){ 
     if(change == S[i]){
